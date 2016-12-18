d5405e9cfa718885b9440558a5ce15cddde3ec8e
Igor Paprzycki <igypap@gmail.com>
2016-12-18 15:46:18 +0100

    Added menu button adding new note

--- app/src/main/java/com/igypap/todolist/TodoListActivity.java
+++ app/src/main/java/com/igypap/todolist/TodoListActivity.java
@@ -1,9 +1,12 @@
 package com.igypap.todolist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -16,10 +19,27 @@ public class TodoListActivity extends AppCompatActivity {
     RecyclerView mTodoList;
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_item_create) {
+            Intent createTaskIntent = new Intent(this, TaskCreateActivity.class);
+            startActivity(createTaskIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_todolist, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
         ButterKnife.bind(this);
+
 
         //1. Layout of to do ist elements (linear vertical)
         mTodoList.setLayoutManager(new LinearLayoutManager(this));
